c0d61e581aec3128b59d9ad981de3f09679a786b
vemerion <vemerion@gmail.com>
2023-06-28 20:45:32 +0200

    Generate special portal at spawn

--- src/main/java/mod/surviving_the_aftermath/event/ForgeEventSubscriber.java
+++ src/main/java/mod/surviving_the_aftermath/event/ForgeEventSubscriber.java
@@ -3,10 +3,21 @@ package mod.surviving_the_aftermath.event;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 import mod.surviving_the_aftermath.Main;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockIgnoreProcessor;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -29,5 +40,57 @@ public class ForgeEventSubscriber {
 				RAIDS.add(raid.getId());
 			}
 		}
+	}
+
+	@SubscribeEvent
+	public static void generateStructure(PlayerLoggedInEvent event) {
+		if (event.getEntity().level() instanceof ServerLevel level) {
+			var data = level.getDataStorage().computeIfAbsent(nbt -> SpawnStructure.CODEC
+					.parse(NbtOps.INSTANCE, nbt.get("SpawnStructure")).getOrThrow(false, s -> {
+					}), () -> new SpawnStructure(false), Main.MODID + "_SpawnStructure");
+			data.generate(level);
+		}
+	}
+
+	private static class SpawnStructure extends SavedData {
+
+		private static final ResourceLocation STRUCTURE = new ResourceLocation(Main.MODID, "nether_invasion_portal");
+
+		public static final Codec<SpawnStructure> CODEC = RecordCodecBuilder
+				.create(instance -> instance.group(Codec.BOOL.fieldOf("created").forGetter(SpawnStructure::isCreated))
+						.apply(instance, SpawnStructure::new));
+
+		private boolean created;
+
+		public SpawnStructure(boolean created) {
+			this.created = created;
+		}
+
+		public boolean isCreated() {
+			return created;
+		}
+
+		private void generate(ServerLevel level) {
+			if (created || !level.dimension().equals(Level.OVERWORLD))
+				return;
+
+			created = true;
+			setDirty();
+
+			level.getStructureManager().get(STRUCTURE).ifPresent(template -> {
+				var pos = level.getSharedSpawnPos();
+				template.placeInWorld(level, pos, pos,
+						new StructurePlaceSettings().addProcessor(BlockIgnoreProcessor.STRUCTURE_BLOCK), level.random,
+						2);
+			});
+		}
+
+		@Override
+		public CompoundTag save(CompoundTag pCompoundTag) {
+			pCompoundTag.put("SpawnStructure", CODEC.encodeStart(NbtOps.INSTANCE, this).getOrThrow(false, s -> {
+			}));
+			return pCompoundTag;
+		}
+
 	}
 }
