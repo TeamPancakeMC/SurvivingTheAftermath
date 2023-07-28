648a23a3955d80521c776706c179f0c21f15431f
uuz <130070346+sx-uuz@users.noreply.github.com>
2023-07-28 22:40:05 +0800

    Update ModStructureSets.java

--- src/main/java/mod/surviving_the_aftermath/init/ModStructureSets.java
+++ src/main/java/mod/surviving_the_aftermath/init/ModStructureSets.java
@@ -32,34 +32,34 @@ public class ModStructureSets {
         int[] wagonCargoSalt = new int[]{0, 1365330241, 161766032, 1733648479, 2081980804, 418595573, 947847921};
         int[] burntSalt = new int[] {0, 104706480, 705540248, 1838764692, 1669956657, 1334899266, 2119656745};
         context.register(CITY_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(CITY),
-                new RandomSpreadStructurePlacement(32, 8, RandomSpreadType.LINEAR, 2057068235)));
+                new RandomSpreadStructurePlacement(80, 40, RandomSpreadType.LINEAR, 2057068235)));
         context.register(HOUSE_OF_SAKURA_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(HOUSE_OF_SAKURA),
-                new RandomSpreadStructurePlacement(32, 8, RandomSpreadType.LINEAR, 1712650656)));
+                new RandomSpreadStructurePlacement(80, 40, RandomSpreadType.LINEAR, 1712650656)));
         context.register(NETHER_RAID_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(NETHER_RAID),
-                new RandomSpreadStructurePlacement(32, 8, RandomSpreadType.LINEAR, 1629143766)));
+                new RandomSpreadStructurePlacement(100, 40, RandomSpreadType.LINEAR, 1629143766)));
         context.register(CAMP_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(CAMP),
-                new RandomSpreadStructurePlacement(3, 2, RandomSpreadType.LINEAR, 1310135130)));
+                new RandomSpreadStructurePlacement(100, 40, RandomSpreadType.LINEAR, 1310135130)));
         context.register(LOGS_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(LOGS),
-                new RandomSpreadStructurePlacement(3, 1, RandomSpreadType.LINEAR, 1321561321)));
+                new RandomSpreadStructurePlacement(90, 40, RandomSpreadType.LINEAR, 1321561321)));
         context.register(TENT_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(TENT),
-                new RandomSpreadStructurePlacement(4, 2, RandomSpreadType.LINEAR, 1653821652)));
+                new RandomSpreadStructurePlacement(90, 40, RandomSpreadType.LINEAR, 1653821652)));
         context.register(BRICK_WELL_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(BRICK_WELL),
-                new RandomSpreadStructurePlacement(2, 1, RandomSpreadType.LINEAR, 1561323135)));
+                new RandomSpreadStructurePlacement(95, 40, RandomSpreadType.LINEAR, 1561323135)));
         context.register(CONSTRUCTION_1_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(CONSTRUCTION_1),
-                new RandomSpreadStructurePlacement(5, 3, RandomSpreadType.LINEAR, 761313131)));
+                new RandomSpreadStructurePlacement(100, 40, RandomSpreadType.LINEAR, 761313131)));
         context.register(CONSTRUCTION_2_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(CONSTRUCTION_2),
-                new RandomSpreadStructurePlacement(5, 3, RandomSpreadType.LINEAR, 498231431)));
+                new RandomSpreadStructurePlacement(85, 40, RandomSpreadType.LINEAR, 498231431)));
         context.register(COBBLESTONE_PILE_SET, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(COBBLESTONE_PILE),
-                new RandomSpreadStructurePlacement(3, 1, RandomSpreadType.LINEAR, 795416633)));
+                new RandomSpreadStructurePlacement(75, 40, RandomSpreadType.LINEAR, 795416633)));
         for (int i = 1; i <= 6; i++) {
             ResourceKey<StructureSet> wagonCargoSet = register("wagon_cargo" + i);
             ResourceKey<StructureSet> burntSet = register("burnt_structure" + i);
             ResourceKey<Structure> wagonCargo = ModStructures.register("wagon_cargo" + i);
             ResourceKey<Structure> burnt = ModStructures.register("burnt_structure" + i);
             context.register(wagonCargoSet, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(wagonCargo),
-                    new RandomSpreadStructurePlacement(3, 2, RandomSpreadType.LINEAR, wagonCargoSalt[i])));
+                    new RandomSpreadStructurePlacement(100, 40, RandomSpreadType.LINEAR, wagonCargoSalt[i])));
             context.register(burntSet, new StructureSet(context.lookup(Registries.STRUCTURE).getOrThrow(burnt),
-                    new RandomSpreadStructurePlacement(3, 2, RandomSpreadType.LINEAR, burntSalt[i])));
+                    new RandomSpreadStructurePlacement(80, 40, RandomSpreadType.LINEAR, burntSalt[i])));
         }
     }
 
