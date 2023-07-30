ad53f81a1876fc4c8c0d3717b0d06f73932bda88
vemerion <vemerion@gmail.com>
2023-07-30 08:39:26 +0200

    Fill chests

--- src/main/java/mod/surviving_the_aftermath/structure/AbstractStructure.java
+++ src/main/java/mod/surviving_the_aftermath/structure/AbstractStructure.java
@@ -1,26 +1,32 @@
 package mod.surviving_the_aftermath.structure;
 
+import java.util.Optional;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.WorldgenRandom;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraft.world.level.levelgen.structure.TemplateStructurePiece;
+import net.minecraft.world.level.levelgen.structure.pieces.PiecesContainer;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceSerializationContext;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceType;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockIgnoreProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
-
-import java.util.Optional;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 
 public abstract class AbstractStructure extends Structure {
 
@@ -43,6 +49,16 @@ public abstract class AbstractStructure extends Structure {
             pieces.addPiece(new AbstractStructure.Piece(this.pieceType(), context.structureTemplateManager(), this.location(), pos, rotation));
         });
     }
+    
+	@Override
+	public void afterPlace(WorldGenLevel pLevel, StructureManager pStructureManager, ChunkGenerator pChunkGenerator,
+			RandomSource pRandom, BoundingBox pBoundingBox, ChunkPos pChunkPos, PiecesContainer pPieces) {
+		BlockPos.betweenClosedStream(pBoundingBox).forEach(p -> {
+			if (pLevel.getBlockEntity(p) instanceof RandomizableContainerBlockEntity chest) {
+				chest.setLootTable(BuiltInLootTables.DESERT_PYRAMID, pRandom.nextLong());
+			}
+		});
+	}
 
     @Override
     public abstract StructureType<?> type();
