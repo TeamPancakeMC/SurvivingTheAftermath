6d1f3643e5939b7eef5349989e7d09cba92bac84
glyceryl <494192175@qq.com>
2023-08-05 10:28:50 +0800

    Delete "VillagerMixin" from mixin config temporary.

--- src/main/java/mod/surviving_the_aftermath/mixin/VillagerMixin.java
+++ src/main/java/mod/surviving_the_aftermath/mixin/VillagerMixin.java
@@ -6,32 +6,29 @@ import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.trading.MerchantOffer;
-import net.minecraft.world.item.trading.MerchantOffers;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Villager.class)
-public abstract class VillagerMixin extends AbstractVillager{
+@Mixin(value = Villager.class, remap = false)
+public abstract class VillagerMixin extends AbstractVillager {
 
-
-    public VillagerMixin(EntityType<? extends AbstractVillager> pEntityType, Level pLevel) {
-        super(pEntityType, pLevel);
+    public VillagerMixin(EntityType<? extends AbstractVillager> entityType, Level level) {
+        super(entityType, level);
     }
 
     //updateSpecialPrices
-    @Inject( method = "updateSpecialPrices",at = @At("RETURN"), remap=false)
-    private void updateSpecialPrices(Player pPlayer, CallbackInfo ci) {
-        MerchantOffers offers = this.getOffers();
-
-        if (pPlayer.hasEffect(ModMobEffects.COWARDICE.get())) {
-            for (MerchantOffer offer : offers) {
+    @Inject(method = "updateSpecialPrices", at = @At("TAIL"), remap = false)
+    private void updateSpecialPrices(Player player, CallbackInfo ci) {
+        if (player.hasEffect(ModMobEffects.COWARDICE.get())) {
+            for (MerchantOffer offer : this.getOffers()) {
                 double d0 = 0.3D + 0.0625D;
                 int j = (int) Math.floor(d0 * (double) offer.getBaseCostA().getCount());
                 offer.addToSpecialPriceDiff(Math.max(j, 1));
             }
         }
     }
+
 }
